85e02699e77787dc7e7a71ffe29d30c2cffc648c
Raisess <raisechif@gmail.com>
2023-06-23 09:00:08 -0300

    feat: custom image version

--- src/image.py
+++ src/image.py
@@ -3,13 +3,18 @@ from api.base import Api
 class Image:
   def __init__(self, provider: Api.Image, name: str):
     self.__provider = provider
-    self.__name = name
+    splitted = name.split(":")
+    self.__name = splitted[0]
+    self.__version = splitted[1] if len(splitted) > 1 else "latest"
 
   def name(self) -> str:
     return self.__name
 
+  def version(self) -> str:
+    return self.__version
+
   def pull(self) -> None:
-    self.__provider.pull(self.__name, "latest")
+    self.__provider.pull(self.name(), self.version())
 
   def delete(self) -> None:
-    self.__provider.delete(self.__name)
+    self.__provider.delete(self.name())
